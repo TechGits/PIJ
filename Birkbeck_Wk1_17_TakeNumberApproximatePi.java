05ce0a8d867a7ef8323312dbb04d6a1f819d709c
Ricki Angel <rickihippy@hotmail.com>
2023-10-10 11:35:51 +0100

    Added annotations

--- Birkbeck_Wk1_17_TakeNumberApproximatePi.java
+++ Birkbeck_Wk1_17_TakeNumberApproximatePi.java
@@ -8,13 +8,13 @@ public class Birkbeck_Wk1_17_TakeNumberApproximatePi {
         double piApproximation = 0.0;
 
 
-        for (int k = 0; k < terms; k++) {
-            double term = (k % 2 == 0) ? 1.0 : -1.0;
+        for (int k = 0; k < terms; k++) { // Runs loop for up to number entered by user.
+            double term = (k % 2 == 0) ? 1.0 : -1.0; // Ternary. If the iteration of k is even, it sets it to positive (1), else sets sign to negative.
             piApproximation += term / (2 * k + 1);
         }
 
         piApproximation *= 4;
 
-        System.out.println("Approximation of π: " + piApproximation);
+        System.out.println("Approximation of pi: " + piApproximation);
     }
 }
