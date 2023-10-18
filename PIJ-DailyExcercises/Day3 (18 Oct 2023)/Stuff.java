d4a932f9c7463b2a69f328cfdadad4ce0a8d57f4
Ricki Angel <rangel03@student.bbk.ac.uk>
2023-10-18 18:08:32 +0100

    Day 3 Excercises - 18 Oct

---
+++ PIJ-DailyExcercises/Day3 (18 Oct 2023)/Stuff.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Stuff {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You have Three Wishes\n");
+        for (int i = 1; i < 4; i++) {
+        System.out.print("Enter wish " +i +": ");
+        String yourText = scanner.nextLine();
+            
+        }
+    }}
