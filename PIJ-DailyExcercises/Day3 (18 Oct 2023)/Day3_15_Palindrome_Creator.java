ba18e25bd1ea69c9c7f18e05b86666cc6fbac9a4
Ricki Angel <rangel03@student.bbk.ac.uk>
2023-10-19 07:40:55 +0100

    Updated 08:00 19 Oct

--- PIJ-DailyExcercises/Day3 (18 Oct 2023)/Day3_15_Palindrome_Creator.java
+++ PIJ-DailyExcercises/Day3 (18 Oct 2023)/Day3_15_Palindrome_Creator.java
@@ -2,14 +2,33 @@ import java.util.Scanner;
 
 public class Day3_15_Palindrome_Creator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a phrase: ");
-        String yourText = scanner.nextLine();
-        
-        for (int i = 0; i <yourText.length(); i++){
-            String palinDrome = yourText
+            
+            String input = "ici";
 
+            if (isPalindrome(input)) {
+                System.out.println(input + " is a palindrome.");
+            } else {
+                System.out.println(input + " is not a palindrome.");
+            }
         }
+
+        // Function to check if a string is a palindrome
+        public static boolean isPalindrome(String str) {
+            // Remove spaces and convert to lowercase for case-insensitive comparison
+            str = str.replaceAll("\\s", "").toLowerCase();
+            
+            int left = 0;
+            int right = str.length() - 1;
+            System.out.println(right);
+            while (left < right) {
+                if (str.charAt(left) != str.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
     }
     
 }
