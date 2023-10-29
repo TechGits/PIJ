eee6ce8adfaae856a90bf1b72bd17a6d4d06b681
Ricki Angel <rangel03@student.bbk.ac.uk>
2023-10-29 17:42:02 +0000

    Day 4, fixed Excercize 4c Bin to Dec

--- PIJ-DailyExcercises/Day4 (25 Oct 2023)/Day4_4c_Bin2Dec.java
+++ PIJ-DailyExcercises/Day4 (25 Oct 2023)/Day4_4c_Bin2Dec.java
@@ -1,13 +1,28 @@
 public class Day4_4c_Bin2Dec {
     public static void main(String[] args) {
-        System.out.println(power(2,16));
-
+        int conversion = dec2Num("10000");
+        System.out.println(conversion);
+    }
+    static int dec2Num(String yourString) {
+        int decConv = 0;
+        int binaryLength = yourString.length();
+        for (int i = 0; i < binaryLength; i++) {
+            char digit = yourString.charAt(i);
+            if (digit == '1') {
+                decConv += power(2, binaryLength - 1 - i);
+            }
+        }
+        return decConv;
     }
 
-    static int power(int base, int exponent){
-        int result = 1;
-        for (int i = 0; i < exponent ; i++) {
-            result *= base;
-        } return result;
-    }
+        static int power ( int base, int exponent){
+            int result = 1;
+            for (int i = 0; i < exponent; i++) {
+                result *= base;
+            }
+            return result;
+        }
+
+
+
 }
