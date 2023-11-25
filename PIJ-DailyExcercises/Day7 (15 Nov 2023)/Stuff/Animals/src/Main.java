292f72684e2cfd2845af51a5bf9dfe135f22e49b
Ricki Angel <rangel03@student.bbk.ac.uk>
2023-11-25 18:51:54 +0000

    worked on my on Animal interface/class project

---
+++ PIJ-DailyExcercises/Day7 (15 Nov 2023)/Stuff/Animals/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog rottweiler = new Dog("Border Collie","Rover",8);
+
+        rottweiler.stats();
+        Dog pitBull = new Dog("Pitbull","Winston",12);
+        pitBull.stats();
+    }
+
+
+}
