6d592e13f8e1b5ceb733bbdff7a723de5e19c967
Ricki Angel <rickihippy@hotmail.com>
2024-04-15 13:31:55 +0100

    Threads Done

--- PIJ-DailyExcercises/BASICS/Concurrency_Threading_Two_Threads_At_Same_Time/src/main/java/org/example/Main.java
+++ PIJ-DailyExcercises/BASICS/Concurrency_Threading_Two_Threads_At_Same_Time/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Creating and starting a thread by implementing the Runnable interface. the run() method is override to pass in parameters
-        MyRunnable runnable1 = new MyRunnable("One");
+        MyRunnable runnable1 = new MyRunnable("#1");
 
         // Creating a Thread object, to use the start() method
         Thread thread1 = new Thread(runnable1);             // Usses the MyRunnable class (with it's fixed override for loop (1 < 10.
@@ -14,43 +14,42 @@ public class Main {
         thread1.start();
         thread2.start();
         thread3.start();
-
     }
                 // THREAD THREE WITH LAMBDAS
                 // --------------------------
 
     //BESPOKE LAMDA 2
     static Runnable runnableLambdaMethod2 = () -> {
-        String threadName = "Two";
+        String threadName2 = "#2";
 
-        for (int i = 2000; i <= 3000; i+= 100) {
+        for (int i = 50; i < 55; i++) {
             try {
                 Thread.sleep(500);
-                System.out.println("Thread "+threadName+": "+ i);
+                System.out.println("Thread "+threadName2+": "+ i);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
         }
-        System.out.println("Thread "+threadName+" is finished.");
+        System.out.println("Thread "+threadName2+" is finished.");
     };
 
 
 
     //BESPOKE LAMDA 3
                 static Runnable runnableLambdaMethod3 = () -> {
-                    String threadName = "Three";
+                    String threadName3 = "#3";
 
-                    for (int i = 50; i < 60; i++) {
+                    for (int i = 2000; i <= 3000; i+=200) {
                         try {
                             Thread.sleep(500);
-                            System.out.println("Thread "+threadName+": "+ i);
+                            System.out.println("Thread "+threadName3+": "+ i);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
 
                     }
-                    System.out.println("Thread "+threadName+" is finished.");
+                    System.out.println("Thread "+threadName3+" is finished.");
                 };
 
     }
--- PIJ-DailyExcercises/BASICS/Concurrency_Threading_Two_Threads_At_Same_Time/src/main/java/org/example/MyRunnable.java
+++ PIJ-DailyExcercises/BASICS/Concurrency_Threading_Two_Threads_At_Same_Time/src/main/java/org/example/MyRunnable.java
@@ -1,22 +1,22 @@
 package org.example;
 
 public class MyRunnable implements Runnable {
-    private String threadName;
+    private String threadName1;
 
     public MyRunnable(String threadName)    {
-    this.threadName =threadName;
+    this.threadName1 =threadName;
 
 }
     @Override
     public void run(){
-        for (int i = 0; i < 10 ; i++) {
-            System.out.println("Thread "+threadName+": "+ i);
+        for (int i = 0; i <= 5 ; i++) {
+            System.out.println("Thread "+threadName1+": "+ i);
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }        System.out.println("Thread "+threadName+" is finished.");
+        }        System.out.println("Thread "+threadName1+" is finished.");
 
 
     }
