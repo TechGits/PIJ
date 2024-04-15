d2b16a25e895b7463d056ea44a20188aae6d3725
Ricki Angel <rickihippy@hotmail.com>
2024-04-15 09:43:56 +0100

    Created 2 Runnable objects, 2 with MyRunnable, 1 with a becpoke Runnable implementation with different variables

--- PIJ-DailyExcercises/BASICS/Concurrency_Threading_Two_Threads_At_Same_Time/src/main/java/org/example/Main.java
+++ PIJ-DailyExcercises/BASICS/Concurrency_Threading_Two_Threads_At_Same_Time/src/main/java/org/example/Main.java
@@ -3,18 +3,42 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        // Creating and starting a thread by implementing the Runnable interface. the run() mehtod is overriden to pass in parameters
+        // Creating and starting a thread by implementing the Runnable interface. the run() method is override to pass in parameters
         MyRunnable runnable1 = new MyRunnable("One");
         MyRunnable runnable2 = new MyRunnable("Two");
+
 
         // Creating a Thread object, to use the start() methood
         Thread thread1 = new Thread(runnable1);
         Thread thread2 = new Thread(runnable2);
+        Thread thread3 = new Thread(runnableLambdaMethod); // Calls in my static runnableLambdaMethod static method, with different variables.
 
         thread1.start();
         thread2.start();
+        thread3.start();
 
+    }
+                // THREAD THREE WITH LAMBDAS
+                // --------------------------
+
+                static Runnable runnableLambdaMethod = () -> {
+                    String threadName = "Three";
+
+                    for (int i = 50; i < 60; i++) {
+                        try {
+                            Thread.sleep(500);
+                            System.out.println("Thread "+threadName+": "+ i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    System.out.println("Thread "+threadName+" is finished.");
+                };
 
     }
 
-}
+
+
+
+
