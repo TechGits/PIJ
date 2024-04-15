ad61ec844fac588796d394e1c4b1b612d9ccc121
Ricki Angel <rickihippy@hotmail.com>
2024-04-15 10:10:38 +0100

    Concurrency with two Lambdas

--- PIJ-DailyExcercises/BASICS/Concurrency_Threading_Two_Threads_At_Same_Time/src/main/java/org/example/Main.java
+++ PIJ-DailyExcercises/BASICS/Concurrency_Threading_Two_Threads_At_Same_Time/src/main/java/org/example/Main.java
@@ -5,13 +5,11 @@ public class Main {
 
         // Creating and starting a thread by implementing the Runnable interface. the run() method is override to pass in parameters
         MyRunnable runnable1 = new MyRunnable("One");
-        MyRunnable runnable2 = new MyRunnable("Two");
 
-
-        // Creating a Thread object, to use the start() methood
-        Thread thread1 = new Thread(runnable1);
-        Thread thread2 = new Thread(runnable2);
-        Thread thread3 = new Thread(runnableLambdaMethod); // Calls in my static runnableLambdaMethod static method, with different variables.
+        // Creating a Thread object, to use the start() method
+        Thread thread1 = new Thread(runnable1);             // Usses the MyRunnable class (with it's fixed override for loop (1 < 10.
+        Thread thread2 = new Thread(runnableLambdaMethod2); // Calls in my static runnableLambdaMethod2 static method, with different variables.
+        Thread thread3 = new Thread(runnableLambdaMethod3); // Calls in my static runnableLambdaMethod3 static method, with different variables.
 
         thread1.start();
         thread2.start();
@@ -21,7 +19,26 @@ public class Main {
                 // THREAD THREE WITH LAMBDAS
                 // --------------------------
 
-                static Runnable runnableLambdaMethod = () -> {
+    //BESPOKE LAMDA 2
+    static Runnable runnableLambdaMethod2 = () -> {
+        String threadName = "Two";
+
+        for (int i = 2000; i <= 3000; i+= 100) {
+            try {
+                Thread.sleep(500);
+                System.out.println("Thread "+threadName+": "+ i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        System.out.println("Thread "+threadName+" is finished.");
+    };
+
+
+
+    //BESPOKE LAMDA 3
+                static Runnable runnableLambdaMethod3 = () -> {
                     String threadName = "Three";
 
                     for (int i = 50; i < 60; i++) {
